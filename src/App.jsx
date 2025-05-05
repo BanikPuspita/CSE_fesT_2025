b8e9a608a795443e64d02a60bb7ee068e6ca8b00
puspita <puspitabanik213@gmail.com>
2025-05-05 12:01:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>CSE FEST</h1>
+      <button className="btn btn-xs">Xsmall</button>
+<button className="btn btn-sm">Small</button>
+<button className="btn">Medium</button>
+<button className="btn btn-lg">Large</button>
+<button className="btn btn-xl">Xlarge</button>
+    </div>
+  );
+};
+
+export default App;
